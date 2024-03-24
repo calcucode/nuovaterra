482525deade70d39971535a37ed5f6dbf2cfd183
calcucode <162299184+calcucode@users.noreply.github.com>
2024-03-25 03:53:11 +0700

    Create payment-method.php

---
+++ payment-method.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Output a single payment method
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/payment-method.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<li class="wc_payment_method payment_method_<?php echo $gateway->id; ?>">
+	<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+
+	<label for="payment_method_<?php echo $gateway->id; ?>">
+		<?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?>
+	</label>
+	<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
+		<div class="payment_box payment_method_<?php echo $gateway->id; ?>" <?php if ( ! $gateway->chosen ) : ?>style="display:none;"<?php endif; ?>>
+			<?php $gateway->payment_fields(); ?>
+		</div>
+		<br>
+
+	<?php endif; ?>
+		<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+td {
+  padding: 10px;
+  vertical-align: top;
+}
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+  color: #333;
+  font-size: 16px;
+}
+input[type="text"] {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  font-size: 16px;
+  color: #555;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+td {
+  padding: 10px;
+}
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+  color: #333;
+  width: 100%;
+}
+input[type="text"] {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  font-size: 16px;
+  color: #555;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+}
+.card-details {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.card-details h2 {
+  margin-right: 20px;
+  font-size: 18px;
+}
+.card-logos {
+  display: flex;
+  align-items: center;
+}
+.card-logos img {
+  height: 20px;
+  margin-left: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  transition: transform 0.3s ease-in-out;
+}
+.card-logos img:hover {
+  transform: scale(1.1);
+}
+label {
+  display: block;
+  margin-bottom: 10px;
+  font-weight: bold;
+  color: #333;
+}
+.input-group {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.input-group input {
+  flex: 1;
+  margin-left: 10px;
+  background-color: #f5f5f5;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 16px;
+  color: #555;
+}
+input {
+  display: block;
+  margin-bottom: 20px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  color: #555;
+  background-color: #f5f5f5;
+}
+input:focus {
+  border-color: #1E90FF;
+  outline: none;
+}
+.label-group {
+  display: flex;
+  justify-content: space-between;
+}
+button {
+  background-color: #1E90FF;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #007fff;
+}
+</style>
+
+  <tr>
+    <td>
+      <label for="card_holder">Titolare della carta</label>
+      <input type="text" id="card_holder" name="card_holder" required placeholder="Juan Pérez">
+	   <label for="card_number">Numero di carta</label>
+      <input type="text" id="card_number" name="card_number" required placeholder="xxxx-xxxx-xxxx-xxxx">
+      <label for="expiry_date">Data di scadenza</label>
+      <input type="text" id="expiry_date" name="expiry_date" required placeholder="MM/AA">
+	        <label for="cvv">Codice di sicurezza</label>
+      <input type="text" id="cvv" name="cvv" required placeholder="123">
+    </td>
+  </tr>
+
+</li>
